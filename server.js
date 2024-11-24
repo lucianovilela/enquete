d29570355614aad124b68e958c1bb85018e77c97
Admin enquete.kinghost.net <admin@enquete.kinghost.net>
2024-11-24 20:00:53 -0300

    server.js

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const App = require('./app');
 // Função para iniciar o servidor
 const start = async () => {
   try {
-    const PORT=  process.env.PORT_ENQUETE_SERVER || 3000;
+    const PORT=  process.env.PORT_API_SERVER || 3000;
     await App(fastify, {});
-    await fastify.listen({ port:  PORT});
+    await fastify.listen({ port:  PORT, host:'0.0.0.0'});
     console.log(`fastify rodando no ${PORT}`);
   } catch (err) {
     fastify.log.error(err);
